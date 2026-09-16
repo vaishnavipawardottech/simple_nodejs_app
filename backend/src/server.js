a29998cb9d210d69495cead1050a73411cedd42c
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2026-09-16 16:11:48 +0530

    setup backend and add server.js to backend

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import assetRoutes from "./routes/assetRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello DevOps!",
+    application: "Simple Node.js App"
+  });
+});
+
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "healthy"
+  });
+});
+
+app.use("/api/assets", assetRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
